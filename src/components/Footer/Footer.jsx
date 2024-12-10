19586092c678872d29130f645902d36b3d64ce9b
VIP-WABUG <114746072+VIP-WABUG@users.noreply.github.com>
2024-12-11 02:33:01 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import GithubIco from '../../Media/github-icon.svg';
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-left">Made with ❤️ by Keshav</div>
+            <div className="footer-left">Made with Lann V1.01</div>
             <div className="footer-right">
-                <a href="https://github.com/kecav/igdownloader"  target="_blank" rel="noreferrer">
+                <a href="https://github.com/VIP-WABUG"  target="_blank" rel="noreferrer">
                     <img src={GithubIco} className="github-icon" alt="Github redirect"/>
                 </a>
             </div>
